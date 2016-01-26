4ea45202554dccf25fa627eee3ff50ad30781ec4
Toh Yong Cheng <tohyongcheng@gmail.com>
2016-01-27 00:19:23 +0800

    commit all for day one

---
+++ day2/warmup_ex.rb
@@ -0,0 +1,14 @@
+# Given the following data structures. Write a program that moves the information from the array into the empty hash that applies to the correct person.
+
+
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+
+contacts["Joe Smith"][:email] = contact_data[0][0]
+contacts["Joe Smith"][:address] = contact_data[0][1]
+contacts["Joe Smith"][:phone] = contact_data[0][2]
+contacts["Sally Johnson"][:email] = contact_data[1][0]
+contacts["Sally Johnson"][:address] = contact_data[1][1]
+contacts["Sally Johnson"][:phone] = contact_data[1][2]
