1d5985acba5f25ea9b3691f3918a540f1311e482
Toh Yong Cheng <tohyongcheng@gmail.com>
2016-01-26 14:02:19 +0800

    add soln for rps

--- day2/games_on_rails/app/controllers/rps_controller.rb
+++ day2/games_on_rails/app/controllers/rps_controller.rb
@@ -15,5 +15,21 @@ class RpsController < ApplicationController
 
   def generate_results(user_weapon, server_weapon)
     # Your logic for determining results
+    if user_weapon == server_weapon
+      return "Draw"
+    end
+
+    winner_of = {
+      "rock" => "paper",
+      "paper" => "scissors",
+      "scissors" => "rock"
+    }
+
+    if winner_of[server_weapon] == user_weapon
+      return "Win"
+    else
+      return "Lose"
+    end
+
   end
 end
